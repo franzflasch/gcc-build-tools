8635d7fe7f40b2ad6df6ff10f50632fa4d5c4c42
Franz Flasch <franz.flasch@gmx.at>
2018-09-12 15:19:39 +0000

    Unify baremetal and linux builds

--- configs/target_config.sh
+++ configs/target_config.sh
@@ -3,153 +3,162 @@
 # have a config function for each architecture, as it is possible
 # to inject different architecure dependent configuration settings for the build here.
 
-function setup_linux_default_config()
+function setup_default_config()
 {
     IS_BARE_METAL=false
 
     # Configuration variables
-    BINUTILS_CONFIGURATION=("--disable-nls"
-                            "--disable-werror"
-                            "--target=${TARGET}"
-                            "--prefix=${INSTALL}")
+    BINUTILS_BASE_CONFIG=("--target=${TARGET}"
+                          "--prefix=${INSTALL}"
+                          "--disable-nls"
+                          "--disable-werror")
 
-    GCC_STAGE_1_CONFIGURATION=("--disable-libmpx" # Only for intel x86
-                               "--disable-multilib"
-                               "--enable-languages=c"
-                               "--target=${TARGET}"
-                               "--prefix=${INSTALL}")
+    GCC_BASE_CONFIG=("--target=${TARGET}"
+                     "--prefix=${INSTALL}"
+                     "--disable-nls"
+                     "--enable-languages=c")
 
-    GLIBC_STAGE_1_CONFIGURATION=("--prefix=${INSTALL}/${TARGET}"
-                                 "--with-headers=${INSTALL}/${TARGET}/include"
-                                 "--host=${TARGET}"
-                                 "--target=${TARGET}"
-                                 "libc_cv_forced_unwind=yes"
-                                 "libc_cv_c_cleanup=yes")
-
-    GCC_STAGE_FINAL_CONFIGURATION=("--disable-libmpx" # Only for intel x86
-                                   "--disable-multilib"
-                                   "--enable-languages=c,c++"
-                                   "--target=${TARGET}"
-                                   "--prefix=${INSTALL}")
+    GLIBC_BASE_CONFIG=("--host=${TARGET}"
+                       "--target=${TARGET}"
+                       "--prefix=${INSTALL}/${TARGET}"
+                       "--with-headers=${INSTALL}/${TARGET}/include"
+                       "libc_cv_forced_unwind=yes"
+                       "libc_cv_c_cleanup=yes")
 }
 
 
 # default arm-linux-gnueabi
-function config_arm-linux-gnueabi() {
+function config_arm-linux-gnueabi_arm() {
     local SOURCE="$1"
     local VERSION="$2"
     local TAR_OR_GIT="$3"
 
-    setup_linux_default_config
+    setup_default_config
+
+    # Append additional confis here
+    BINUTILS_CONFIGURATION=("${BINUTILS_BASE_CONFIG[@]}")
+    GCC_CONFIGURATION=("${GCC_BASE_CONFIG[@]}")
+    GLIBC_CONFIGURATION=("${GLIBC_BASE_CONFIG[@]}")
 
     type -t setup_variables_${TAR_OR_GIT}_${SOURCE}_${VERSION} > /dev/null || die "No setup_variables_${TAR_OR_GIT}_${SOURCE}_${VERSION} found!"
     setup_variables_${TAR_OR_GIT}_${SOURCE}_${VERSION}
 }
 
 
-function config_aarch64-linux-gnu() {
+function config_aarch64-linux-gnu_arm64() {
     local SOURCE="$1"
     local VERSION="$2"
     local TAR_OR_GIT="$3"
 
-    setup_linux_default_config
+    setup_default_config
+
+    # Append additional confis here
+    BINUTILS_CONFIGURATION=("${BINUTILS_BASE_CONFIG[@]}")
+    GCC_CONFIGURATION=("${GCC_BASE_CONFIG[@]}")
+    GLIBC_CONFIGURATION=("${GLIBC_BASE_CONFIG[@]}")
 
     type -t setup_variables_${TAR_OR_GIT}_${SOURCE}_${VERSION} > /dev/null || die "No setup_variables_${TAR_OR_GIT}_${SOURCE}_${VERSION} found!"
     setup_variables_${TAR_OR_GIT}_${SOURCE}_${VERSION}
 }
 
 
-function config_i686-linux-gnu() {
+function config_i686-linux-gnu_x86() {
     local SOURCE="$1"
     local VERSION="$2"
     local TAR_OR_GIT="$3"
 
-    setup_linux_default_config
+    setup_default_config
+
+    # Append additional confis here
+    BINUTILS_CONFIGURATION=("${BINUTILS_BASE_CONFIG[@]}")
+    GCC_CONFIGURATION=("${GCC_BASE_CONFIG[@]}"
+                       "--disable-multilib"
+                       "--disable-libmpx"
+                       )
+    GLIBC_CONFIGURATION=("${GLIBC_BASE_CONFIG[@]}")
 
     type -t setup_variables_${TAR_OR_GIT}_${SOURCE}_${VERSION} > /dev/null || die "No setup_variables_${TAR_OR_GIT}_${SOURCE}_${VERSION} found!"
     setup_variables_${TAR_OR_GIT}_${SOURCE}_${VERSION}
 }
 
 
-function config_x86_64-linux-gnu() {
+function config_x86_64-linux-gnu_x86-64() {
     local SOURCE="$1"
     local VERSION="$2"
     local TAR_OR_GIT="$3"
 
-    setup_linux_default_config
+    setup_default_config
+
+    # Append additional confis here
+    BINUTILS_CONFIGURATION=("${BINUTILS_BASE_CONFIG[@]}")
+    GCC_CONFIGURATION=("${GCC_BASE_CONFIG[@]}"
+                       "--disable-multilib"
+                       )
+    GLIBC_CONFIGURATION=("${GLIBC_BASE_CONFIG[@]}")
 
     type -t setup_variables_${TAR_OR_GIT}_${SOURCE}_${VERSION} > /dev/null || die "No setup_variables_${TAR_OR_GIT}_${SOURCE}_${VERSION} found!"
     setup_variables_${TAR_OR_GIT}_${SOURCE}_${VERSION}
 }
 
 
-function config_arm-none-eabi() {
+function config_arm-none-eabi_cm4f() {
     local SOURCE="$1"
     local VERSION="$2"
     local TAR_OR_GIT="$3"
+
+    setup_default_config
 
     IS_BARE_METAL=true
 
-    # Configuration variables
-    BINUTILS_CONFIGURATION=("--target=${TARGET}" 
-                            "--prefix=${INSTALL}" 
-                            "--disable-gdb"
-                            "--with-cpu=cortex-m4"
-                            "--with-fpu=fpv4-sp-d16"
-                            "--with-float=hard"
+    BINUTILS_CONFIGURATION=("${BINUTILS_BASE_CONFIG[@]}" 
+                            "--with-cpu=cortex-m3"
                             "--with-mode=thumb"
                             "--enable-interwork"
+                            "--disable-gdb"
+                            "--with-fpu=fpv4-sp-d16"
+                            "--with-float=hard"
                             "--enable-multilib"
-                            "--with-gnu-as"
-                            "--with-gnu-ld"
-                            "--disable-nls")
+                            )
 
-    GCC_CONFIGURATION=("--target=${TARGET}"
-                       "--prefix=${INSTALL}"
-                       "--with-cpu=cortex-m4"
-                       "--with-fpu=fpv4-sp-d16"
-                       "--with-float=hard" 
+    GCC_CONFIGURATION=("${GCC_BASE_CONFIG[@]}"
+                       "--with-cpu=cortex-m3"
                        "--with-mode=thumb"
                        "--enable-interwork"
-                       "--enable-multilib"
-                       "--enable-languages=c"
                        "--with-system-zlib"
                        "--with-newlib"
-                       "--without-headers"
                        "--disable-shared"
-                       "--disable-nls"
-                       "--with-gnu-as"
-                       "--with-gnu-ld")
+                       "--without-headers"
+                       "--with-fpu=fpv4-sp-d16"
+                       "--with-float=hard"
+                       "--enable-multilib"
+                       )
 
     NEWLIB_CONFIGURATION=("--target=${TARGET}"
                           "--prefix=${INSTALL}"
-                          "--with-cpu=cortex-m4"
-                          "--with-fpu=fpv4-sp-d16"
-                          "--with-float=hard" 
+                          "--with-cpu=cortex-m3"
                           "--with-mode=thumb"
                           "--enable-interwork"
-                          "--enable-multilib"
-                          "--with-gnu-as"
-                          "--with-gnu-ld"
                           "--disable-nls"
-                          "--disable-newlib-supplied-syscalls")
+                          "--disable-newlib-supplied-syscalls"
+                          "--with-fpu=fpv4-sp-d16"
+                          "--with-float=hard"
+                          "--enable-multilib"
+                          )
 
-    GCC_STAGE_2_CONFIGURATION=("--target=${TARGET}"
-                               "--prefix=${INSTALL}"
-                               "--with-cpu=cortex-m4"
-                               "--with-fpu=fpv4-sp-d16"
-                               "--with-float=hard" 
-                               "--with-mode=thumb"
-                               "--enable-interwork"
-                               "--enable-multilib"
-                               "--enable-languages=c"
-                               "--with-system-zlib"
-                               "--with-newlib"
-                               "--disable-shared"
-                               "--disable-nls"
-                               "--with-gnu-as"
-                               "--with-gnu-ld")
+    GCC_FINAL_CONFIGURATION=("${GCC_BASE_CONFIG[@]}"
+                             "--with-cpu=cortex-m3"
+                             "--with-mode=thumb"
+                             "--enable-interwork"
+                             "--with-system-zlib"
+                             "--with-newlib"
+                             "--disable-shared"
+                             "--with-fpu=fpv4-sp-d16"
+                             "--with-float=hard"
+                             "--enable-multilib"
+                             "--enable-languages=c,c++"
+                             )
 
     type -t setup_variables_${TAR_OR_GIT}_${SOURCE}_${VERSION} > /dev/null || die "No setup_variables_${TAR_OR_GIT}_${SOURCE}_${VERSION} found!"
     setup_variables_${TAR_OR_GIT}_${SOURCE}_${VERSION}
 }
+
