c950bc1df2141a563b462d10f71e91e329d6d293
Franz Flasch <franz.flasch@gmx.at>
2018-09-25 13:26:45 +0000

    [RISC-V] Add risc32 baremetal target

--- configs/target_config.sh
+++ configs/target_config.sh
@@ -200,3 +200,55 @@ function config_arm-none-eabi_cm4f() {
     setup_variables_${TAR_OR_GIT}_${SOURCE}_${VERSION}
 }
 
+function config_riscv32-none-elf_riscv-baremetal() {
+    local SOURCE="$1"
+    local VERSION="$2"
+    local TAR_OR_GIT="$3"
+
+    setup_default_config
+
+    IS_BARE_METAL=true
+
+    BINUTILS_CONFIGURATION=(
+        "${BINUTILS_BASE_CONFIG[@]}" 
+        "--with-arch=rv32ima"
+        "--with-abi=ilp32"
+        "--disable-gdb"
+        "--enable-multilib"
+    )
+
+    GCC_CONFIGURATION=(
+        "${GCC_BASE_CONFIG[@]}"
+        "--with-arch=rv32ima"
+        "--with-abi=ilp32"
+        "--with-system-zlib"
+        "--with-newlib"
+        "--disable-shared"
+        "--without-headers"
+        "--enable-multilib"
+    )
+
+    NEWLIB_CONFIGURATION=(
+        "--target=${TARGET}"
+        "--prefix=${INSTALL}"
+        "--with-arch=rv32ima"
+        "--with-abi=ilp32"
+        "--disable-nls"
+        "--disable-newlib-supplied-syscalls"
+        "--enable-multilib"
+    )
+
+    GCC_FINAL_CONFIGURATION=(
+        "${GCC_BASE_CONFIG[@]}"
+        "--with-arch=rv32ima"
+        "--with-abi=ilp32"
+        "--with-system-zlib"
+        "--with-newlib"
+        "--disable-shared"
+        "--enable-multilib"
+        "--enable-languages=c,c++"
+    )
+
+    type -t setup_variables_${TAR_OR_GIT}_${SOURCE}_${VERSION} > /dev/null || die "No setup_variables_${TAR_OR_GIT}_${SOURCE}_${VERSION} found!"
+    setup_variables_${TAR_OR_GIT}_${SOURCE}_${VERSION}
+}
