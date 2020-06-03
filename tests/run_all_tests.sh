53e3352b85a5d270e1f459eafb7254df3653d5c9
Franz Flasch <franz.flasch@gmx.at>
2020-06-03 07:09:04 +0200

    Use single run commands to avoid running into ci step timeout on github

--- tests/run_all_tests.sh
+++ tests/run_all_tests.sh
@@ -6,3 +6,15 @@ docker build -f "$BASEDIR"/dockerfile_debian10 --rm=true .
 IMAGE_ID=$(docker build -f "$BASEDIR"/dockerfile_debian10 --rm=true . | awk '/Successfully built/{print $NF}')
 
 docker rmi "$IMAGE_ID"
+
+RUN /bin/bash -c "/home/script_dir/build-tools-gcc/build-gcc -a arm64 -v 9 $USE_TAR && rm -rf /home/work_dir/*"
+RUN /bin/bash -c "/home/script_dir/build-tools-gcc/build-gcc -a avr8 -v 9 $USE_TAR && rm -rf /home/work_dir/*"
+RUN /bin/bash -c "/home/script_dir/build-tools-gcc/build-gcc -a cortex-m0 -v 9 $USE_TAR && rm -rf /home/work_dir/*"
+RUN /bin/bash -c "/home/script_dir/build-tools-gcc/build-gcc -a cortex-m3 -v 9 $USE_TAR && rm -rf /home/work_dir/*"
+RUN /bin/bash -c "/home/script_dir/build-tools-gcc/build-gcc -a riscv32-baremetal -v 9 $USE_TAR && rm -rf /home/work_dir/*"
+RUN /bin/bash -c "/home/script_dir/build-tools-gcc/build-gcc -a riscv64-baremetal -v 9 $USE_TAR && rm -rf /home/work_dir/*"
+RUN /bin/bash -c "/home/script_dir/build-tools-gcc/build-gcc -a riscv64 -v 9 $USE_TAR && rm -rf /home/work_dir/*"
+RUN /bin/bash -c "/home/script_dir/build-tools-gcc/build-gcc -a x86 -v 9 $USE_TAR && rm -rf /home/work_dir/*"
+RUN /bin/bash -c "/home/script_dir/build-tools-gcc/build-gcc -a x86_64 -v 9 $USE_TAR && rm -rf /home/work_dir/*"
+RUN /bin/bash -c "/home/script_dir/build-tools-gcc/build-gcc -a x86_mingw -v 9 $USE_TAR && rm -rf /home/work_dir/*"
+RUN /bin/bash -c "/home/script_dir/build-tools-gcc/build-gcc -a x86_64_mingw -v 9 $USE_TAR && rm -rf /home/work_dir/*"
--- tests/run_all_tests_tar.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-BASEDIR=$(dirname "$0")
-
-docker build --build-arg USE_TAR="-t" -f "$BASEDIR"/dockerfile_debian10 --rm=true .
-IMAGE_ID=$(docker build --build-arg USE_TAR="-t" -f "$BASEDIR"/dockerfile_debian10 --rm=true . | awk '/Successfully built/{print $NF}')
-
-docker rmi "$IMAGE_ID"
