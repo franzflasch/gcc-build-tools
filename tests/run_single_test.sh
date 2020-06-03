901500ffff8528af14bfad18e41ea89dc24177d3
Franz Flasch <franz.flasch@gmx.at>
2020-06-03 08:00:28 +0200

    fix docker build cmd

--- tests/run_single_test.sh
+++ tests/run_single_test.sh
@@ -4,7 +4,9 @@ BASEDIR=$(dirname "$0")
 ARCH_TO_BUILD="$1"
 ENABLE_TAR_BUILD="$2"
 
-docker build --build-arg ARCH="$ARCH_TO_BUILD" --build-arg USE_TAR="$ENABLE_TAR_BUILD" -f "$BASEDIR"/dockerfile_debian10 --rm=true .
-IMAGE_ID=$(docker build --build-arg USE_TAR="-t" -f "$BASEDIR"/dockerfile_debian10 --rm=true . | awk '/Successfully built/{print $NF}')
+docker_build_cmd="docker build --build-arg ARCH="$ARCH_TO_BUILD" --build-arg USE_TAR="$ENABLE_TAR_BUILD" -f "$BASEDIR"/dockerfile_debian10 --rm=true ."
+
+$docker_build_cmd
+IMAGE_ID=$($docker_build_cmd | awk '/Successfully built/{print $NF}')
 
 docker rmi "$IMAGE_ID"
