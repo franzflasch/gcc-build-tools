6e5fb59a09c50779b271dccaf487c6f7ec6a2e53
Franz Flasch <franz.flasch@gmx.at>
2021-01-09 01:47:18 +0100

    Prepare for workflow artifacts

--- tests/run_single_test.sh
+++ tests/run_single_test.sh
@@ -9,4 +9,8 @@ docker_build_cmd="docker build --build-arg ARCH="$ARCH_TO_BUILD" --build-arg USE
 $docker_build_cmd
 IMAGE_ID=$($docker_build_cmd | awk '/Successfully built/{print $NF}')
 
+CONTAINER_ID=$(docker create ${IMAGE_ID})
+docker cp ${CONTAINER_ID}:/home/toolchain_install .
+docker rm ${CONTAINER_ID}
+
 docker rmi "$IMAGE_ID"
