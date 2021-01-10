63f69454c1b3aad2020e0f36dc1189ae9179c9e8
Franz Flasch <franz.flasch@gmx.at>
2021-01-10 14:40:03 +0100

    do not package x86_64 and arm64, as needs to much memory for github actions

--- tests/run_single_test.sh
+++ tests/run_single_test.sh
@@ -3,8 +3,9 @@
 BASEDIR=$(dirname "$0")
 ARCH_TO_BUILD="$1"
 ENABLE_TAR_BUILD="$2"
+ENABLE_PACKAGE="$3"
 
-docker_build_cmd="docker build --build-arg ARCH="$ARCH_TO_BUILD" --build-arg USE_TAR="$ENABLE_TAR_BUILD" -f "$BASEDIR"/dockerfile_debian10 --rm=true ."
+docker_build_cmd="docker build --build-arg ARCH="$ARCH_TO_BUILD" --build-arg USE_TAR="$ENABLE_TAR_BUILD" --build-arg PACKAGE="$ENABLE_PACKAGE" -f "$BASEDIR"/dockerfile_debian10 --rm=true ."
 
 $docker_build_cmd
 IMAGE_ID=$($docker_build_cmd | awk '/Successfully built/{print $NF}')
