3d64eae15fb74ed283f9a9e416dcbb2fa0d3ce4f
Franz Flasch <franz.flasch@gmx.at>
2023-06-23 12:33:35 +0000

    Initial support for nommu riscv64-uclibc

--- scripts/uclibc-ng.sh
+++ scripts/uclibc-ng.sh
@@ -9,37 +9,20 @@ function build_uclibc_ng() {
     hash -r
 
     header "BUILDING UCLIBC-NG"
-    #mkdir -p "${BUILD_DIR}/build-uclibc-ng"
-    #cd "${BUILD_DIR}/build-uclibc-ng" || die "ERROR: cd to ${BUILD_DIR}/build-uclibc-ng"
     cd "${SOURCES_DIR}/uclibc-ng-${UCLIBC_NG}" || die "ERROR: cd to ${SOURCES_DIR}/uclibc-ng-${UCLIBC_NG}"
-    
+
     call_cmd "make distclean"
 
     touch .config
-    CONFIG_="" "${TOOLS_ROOT_DIR}"/ext/scripts/config -k -e TARGET_riscv64
-    CONFIG_="" "${TOOLS_ROOT_DIR}"/ext/scripts/config -k -d ARCH_USE_MMU
 
-    # This is needed to be able to build libm
-    CONFIG_="" "${TOOLS_ROOT_DIR}"/ext/scripts/config -k -e UCLIBC_HAS_FLOATS
-    CONFIG_="" "${TOOLS_ROOT_DIR}"/ext/scripts/config -k -e UCLIBC_HAS_FPU
-    CONFIG_="" "${TOOLS_ROOT_DIR}"/ext/scripts/config -k -e DO_C99_MATH
-    CONFIG_="" "${TOOLS_ROOT_DIR}"/ext/scripts/config -k -e UCLIBC_HAS_LONG_DOUBLE_MATH
+    $UCLIB_KCONFIG_CONFIGURATION
 
-    CONFIG_="" "${TOOLS_ROOT_DIR}"/ext/scripts/config -k --set-str KERNEL_HEADERS "${INSTALL}/${TARGET}/include"
-    CONFIG_="" "${TOOLS_ROOT_DIR}"/ext/scripts/config -k -e UCLIBC_HAS_EXTRA_COMPAT_RES_STATE
-    CONFIG_="" "${TOOLS_ROOT_DIR}"/ext/scripts/config -k -e UCLIBC_HAS_RESOLVER_SUPPORT
-    CONFIG_="" "${TOOLS_ROOT_DIR}"/ext/scripts/config -k --set-str RUNTIME_PREFIX ""
-    CONFIG_="" "${TOOLS_ROOT_DIR}"/ext/scripts/config -k --set-str DEVEL_PREFIX ""
-    CONFIG_="" "${TOOLS_ROOT_DIR}"/ext/scripts/config -k -d HARDWIRED_ABSPATH
-
-    #call_cmd "make CROSS_COMPILE=${TARGET}- defconfig"
-    
     # uncomment to be able to modify the config
-    # call_cmd "make CROSS_COMPILE=${TARGET}- menuconfig"
-    
-    call_cmd "make CROSS_COMPILE=${TARGET}- oldconfig"
-    call_cmd "make CROSS_COMPILE=${TARGET}-"
-    call_cmd "make CROSS_COMPILE=${TARGET}- PREFIX=${INSTALL}/${TARGET} install"
+    #call_cmd "make CROSS_COMPILE=${TARGET}- menuconfig"
+    #call_cmd "make CROSS_COMPILE=${TARGET}- oldconfig"
+    call_cmd "make CROSS_COMPILE=${TARGET}- olddefconfig"
+    call_cmd "make CROSS_COMPILE=${TARGET}- UCLIBC_EXTRA_CFLAGS=' -g0 -fno-lto'"
+    call_cmd "make CROSS_COMPILE=${TARGET}- PREFIX=${INSTALL}/${TARGET} UCLIBC_EXTRA_CFLAGS=' -g0 -fno-lto' install"
 
     set_build_state "${FUNCNAME[0]}"
 }
