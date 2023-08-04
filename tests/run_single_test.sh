7c60cbff57f5a4998ad4300f6fbc9b259915fe4d
Franz Flasch <franz.flasch@gmx.at>
2023-08-04 20:18:57 +0200

    trying to fix github actions

--- tests/run_single_test.sh
+++ tests/run_single_test.sh
@@ -9,7 +9,7 @@ ENABLE_PACKAGE="$4"
 docker_build_cmd="docker build --build-arg ARCH="$ARCH_TO_BUILD" --build-arg USE_TAR="$ENABLE_TAR_BUILD" --build-arg PACKAGE="$ENABLE_PACKAGE" --build-arg GCC_VERSION="$GCC_VERSION" -f "$BASEDIR"/dockerfile_ubuntu22_04 --rm=true ."
 
 $docker_build_cmd
-IMAGE_ID=$($docker_build_cmd | awk '/Successfully built/{print $NF}')
+IMAGE_ID=$($docker_build_cmd | grep 'writing image' | awk '{print $NF}')
 
 CONTAINER_ID=$(docker create ${IMAGE_ID})
 docker cp ${CONTAINER_ID}:/home/toolchain_install .
