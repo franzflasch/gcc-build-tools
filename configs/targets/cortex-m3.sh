b35b764b6c2237c1f57829509d6e374d4a39bc58
franzflasch <franz.flasch@gmx.at>
2023-08-09 11:31:53 +0200

    newlib-nano: cortex-m3: optimize build for size

--- configs/targets/cortex-m3.sh
+++ configs/targets/cortex-m3.sh
@@ -47,6 +47,17 @@ function config_cortex-m3() {
     )
 
     NEWLIB_NANO_CONFIGURATION=(
+        "CFLAGS_FOR_TARGET=\"-ffunction-sections \
+                             -fdata-sections \
+                             -Os \
+                             -fomit-frame-pointer \
+                             -fno-unroll-loops \
+                             -mabi=aapcs \
+                             -DPREFER_SIZE_OVER_SPEED \
+                             -D__OPTIMIZE_SIZE__ \
+                             -DSMALL_MEMORY \
+                             -D__BUFSIZ__=64 \
+                             -D_REENT_SMALL\""
         "${NEWLIB_NANO_BASE_CONFIGURATION[@]}"
         "--with-cpu=cortex-m3"
         "--with-mode=thumb"
